1843a3098b74e9e7269dc62c949f1d9796c3ad4b
designhaein <137376982+designhaein@users.noreply.github.com>
2023-07-09 01:10:45 +0900

    .

--- layout/js/nav.js
+++ layout/js/nav.js
@@ -1,35 +1,37 @@
-
-
 window.addEventListener('load', function() {
 	let dropdownMenu = document.querySelector('#dropdownMenu');
     let items = document.querySelectorAll('#dropdownMenu li span');
     let openedMenu = document.querySelector('#openedMenu');
     let openMenuItems = document.querySelectorAll('#openedMenu li a');
 
-    console.log(dropdownMenu)
+    function addOn() {
+        let pageLocation = String(document.location).split('/'),
+            fileName = pageLocation[pageLocation.length-1],
+            fileDoc = pageLocation[pageLocation.length-2];
+
+        let projectcategory = String(fileName).split('-'),
+            gg = projectcategory[projectcategory.length-2];
+
+        $('#openedMenu').find('li > a[href*="' + gg + '"]').parent().addClass('on');
+        $('.tabArea').find('a[href="../' + fileDoc + '/' + fileName + '"]').addClass('on');
+        $('#dropdownMenu').find('li[id="menu_' + gg + '"]').addClass('on');
+        $('.nav').find('div.' + gg).addClass('on');
+    }
 
     function handleDropMenu(e) {
         openedMenu.classList.toggle('opened');
         dropdownMenu.classList.toggle('opened');
+        addOn();
     }
-
-    function handleClickMenu(e, index) {
-        console.dir(index);
-        console.dir(e);
-        let targetLi = e.parentElement;
-        targetLi.classList.toggle('on');
-    }
-
 
     items.forEach(e => {
         e.addEventListener("click", function(){
             handleDropMenu(e);
         })
     });
-        
-    openMenuItems.forEach(function(el, index) {
-        el.addEventListener("click", function(){
-            handleClickMenu(el, index);
-        })
-    })
+
+    addOn();
+
 })
+
+
