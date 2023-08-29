88292e45a7268b265899c602cf571ea890f6da38
designhaein <137376982+designhaein@users.noreply.github.com>
2023-08-29 18:58:14 +0900

    230829
    헤더 애드온 적용 안되는 부분 안 될 경우에만 새로고침 하기

--- layout/js/importHTML.js
+++ layout/js/importHTML.js
@@ -1,6 +1,12 @@
+/*
 $(document).ready(function () {
     $("#header").load("../layout/nav.html", function () {
     //페이지 별로 다른 메뉴 css 적용도 가능
         console.log('success');
     });
 })
+*/
+
+$(function() {
+    $("#header").load("../layout/nav.html");
+});
--- layout/js/nav.js
+++ layout/js/nav.js
@@ -1,11 +1,14 @@
+
 window.addEventListener('load', function() {
 	let dropdownMenu = document.querySelector('#dropdownMenu');
     let items = document.querySelectorAll('#dropdownMenu li span');
     let openedMenu = document.querySelector('#openedMenu');
     let openMenuItems = document.querySelectorAll('#openedMenu li a');
+    let curUrl = document.location;
+
 
     function addOn() {
-        let pageLocation = String(document.location).split('/'),
+        let pageLocation = String(curUrl).split('/'),
             fileName = pageLocation[pageLocation.length-1],
             fileDoc = pageLocation[pageLocation.length-2];
 
@@ -16,6 +19,12 @@ window.addEventListener('load', function() {
         $('.tabArea').find('a[href="../' + fileDoc + '/' + fileName + '"]').addClass('on');
         $('#dropdownMenu').find('li[id="menu_' + gg + '"]').addClass('on');
         $('.nav').find('div.' + gg).addClass('on');
+
+        if ($('#openedMenu').find('li > a[href*="' + gg + '"]').parent().length) {
+            console.log('success');
+        } else {
+            location.reload();
+        }
     };
 
     function handleDropMenu(e) {
@@ -31,8 +40,5 @@ window.addEventListener('load', function() {
     });
 
     addOn();
+
 })
-
-
-
-
